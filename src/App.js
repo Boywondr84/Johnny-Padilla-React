1d98314c422bc0454747c0437f8f0a10501daaf4
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2023-03-01 17:26:50 -0600

    added smoke and font for JP name to body. Next add typewriter effect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import MainLanding from './components/Body';
 import NavHeaders from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
       <NavHeaders />
+      <MainLanding />
     </div>
   )
 }
---
+++ src/components/Body/index.js
@@ -0,0 +1,12 @@
+import smoke from "../../assets/smoke-video/Thin_Smoke_86___4K_res.mp4";
+
+function MainLanding() {
+    return (
+        <div className="landing">
+            <video src={smoke} autoPlay muted id="landing-video"></video>
+                <h1>Johnny Padilla</h1>
+        </div>
+    )
+}
+
+export default MainLanding;
