bccafa1130e1ea9b40f6f4c828a9b2eb9b684fbc
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2023-03-03 16:23:28 -0600

    trouble implementing typewriter effect

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,10 +1,13 @@
 import smoke from "../../assets/smoke-video/Thin_Smoke_86___4K_res.mp4";
 
+
 function MainLanding() {
+    
     return (
         <div className="landing">
             <video src={smoke} autoPlay muted id="landing-video"></video>
-                <h1>Johnny Padilla</h1>
+                {/* <h1>Johnny Padilla</h1> */}
+                <div id="intro"></div>
         </div>
     )
 }
