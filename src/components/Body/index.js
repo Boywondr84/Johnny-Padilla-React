b4c046fdaeded5521f65a431c6d7be21b2dd3a75
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2023-03-27 14:46:30 -0500

    implemented typewriter effect on page load. Now just delay the start of typing

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,15 +1,23 @@
 import smoke from "../../assets/smoke-video/Thin_Smoke_86___4K_res.mp4";
-
+import {useState, useEffect} from 'react';
 
 function MainLanding() {
-    
+    const name = "Johnny Padilla";
+    const [textName, setTextName] = useState("");
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setTextName (name.slice(0, textName.length + 1));
+        }, 280);
+        return () => clearTimeout(timeout);
+    }, [textName]);
+
     return (
         <div className="landing">
             <video src={smoke} autoPlay muted id="landing-video"></video>
-                {/* <h1>Johnny Padilla</h1> */}
-                <div id="intro"></div>
+                <h1>{textName}</h1>
         </div>
-    )
+    );
 }
 
 export default MainLanding;
